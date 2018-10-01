9bb5a0d472b114e5d87c643a429376ffc62a98a4
Bhavya Bhatt <bhavyabhatt17@gmail.com>
2018-10-01 15:14:11 +0530

    Update ImageHandler.py

--- ImageHandler.py
+++ ImageHandler.py
@@ -27,7 +27,6 @@ class ImageHandler:
                         index_r = int(8 * self.img[bias_j + j][bias_i + i][0] / 255)
                         index_g = int(8 * self.img[bias_j + j][bias_i + i][1] / 255)
                         index_b = int(8 * self.img[bias_j + j][bias_i + i][2] / 255)
-                        #print(index_r, index_g, index_b)
                         if (index_r == 8):
                             index_r = 7
                         if (index_g == 8):
@@ -39,10 +38,67 @@ class ImageHandler:
                         green[index_g] = green[index_g] + 1
                         blue[index_b] = blue[index_b] + 1
                 
-                #print(red, green, blue)
                 patch = red + green + blue
                 patchvec.append(patch)
         
-        #print(num_row, num_col)      
+        max_row = num_row
+        max_col = num_col
+        
+        col_end = widht - max_col * 32
+        row_end = height - max_row * 32
+        
+        
+        for row in range(0, num_row):
+            bias_i = max_col * 32
+            bias_j = row * 32
+            red = [0, 0, 0, 0, 0, 0, 0, 0]
+            green = [0, 0, 0, 0, 0, 0, 0, 0]
+            blue = [0, 0 ,0 , 0, 0, 0, 0, 0]
+            for i in range(0, col_end):
+                for j in range(0, 32):
+                    index_r = int(8 * self.img[bias_j + j][bias_i + i][0] / 255)
+                    index_g = int(8 * self.img[bias_j + j][bias_i + i][1] / 255)
+                    index_b = int(8 * self.img[bias_j + j][bias_i + i][2] / 255)
+                    if (index_r == 8):
+                        index_r = 7
+                    if (index_g == 8):
+                        index_g = 7
+                    if (index_b == 8):
+                        index_b = 7
+                        
+                    red[index_r] = red[index_r] + int(32 / col_end)
+                    green[index_g] = green[index_g] + int(32 / col_end)
+                    blue[index_b] = blue[index_b] + int(32 / col_end)
+            if (col_end != 0):
+                patch = red + green + blue
+                patchvec.append(patch)
+        
+        for col in range(0, num_col):
+            bias_i = col * 32
+            bias_j = max_row * 32
+            red = [0, 0, 0, 0, 0, 0, 0, 0]
+            green = [0, 0, 0, 0, 0, 0, 0, 0]
+            blue = [0, 0 ,0 , 0, 0, 0, 0, 0]
+            for i in range(0, 32):
+                for j in range(0, row_end):
+                    index_r = int(8 * self.img[bias_j + j][bias_i + i][0] / 255)
+                    index_g = int(8 * self.img[bias_j + j][bias_i + i][1] / 255)
+                    index_b = int(8 * self.img[bias_j + j][bias_i + i][2] / 255)
+                    if (index_r == 8):
+                        index_r = 7
+                    if (index_g == 8):
+                        index_g = 7
+                    if (index_b == 8):
+                        index_b = 7
+                        
+                    red[index_r] = red[index_r] + int(32 / row_end)
+                    green[index_g] = green[index_g] + int(32 / row_end)
+                    blue[index_b] = blue[index_b] + int(32 / row_end)
+            if (row_end != 0):
+                patch = red + green + blue
+                patchvec.append(patch)
+     
         return patchvec
+    
+
         
