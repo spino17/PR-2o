3f9744b3bf283e4f114907f2b7c3def6c1056a42
Bhavya Bhatt <bhavyabhatt17@gmail.com>
2018-10-01 15:13:38 +0530

    Update KMeans.py

--- KMeans.py
+++ KMeans.py
@@ -1,6 +1,7 @@
 import numpy as np
 import random
 import math
+import matplotlib.pyplot as plt
 
 class KMeans:
     k = 0
@@ -62,10 +63,11 @@ class KMeans:
                     new_mean_vec[i] = (1 / N_z[i]) * new_mean_vec[i]
             
             self.mean_vec = new_mean_vec
-            #print(iter_num)
             iter_num = iter_num + 1
+            print("iternation no: ", iter_num)
             
-        print(N_z[0], N_z[1], N_z[2])
+        #print(N_z[0], N_z[1], N_z[2])
+        print(N_z)
         print(self.mean_vec)
             
             
@@ -75,3 +77,21 @@ class KMeans:
             pred_arr = np.append(pred_arr, self.MinDistCluster(x_vec, self.mean_vec))
 
         return pred_arr
+    
+    
+    def BoVW(self, mean_vec, img_vec):
+        bag = np.zeros(self.k)
+        for vec in img_vec:
+            cluster_num = self.MinDistCluster(vec, mean_vec)
+            bag[cluster_num] = bag[cluster_num] + 1
+            
+        return bag
+        
+    
+    def PlotCluster(self): # for 2-D feature vectors
+        y_pred = self.ClusterPredict(self.x_train)
+        plt.scatter(self.x_train[y_pred == 0, 0], self.x_train[y_pred == 0, 1], s = 20, c = 'red', label = 'Cluster 1')
+        plt.scatter(self.x_train[y_pred == 1, 0], self.x_train[y_pred == 1, 1], s = 20, c = 'green', label = 'Cluster 2')
+        plt.scatter(self.x_train[y_pred == 2, 0], self.x_train[y_pred == 2, 1], s = 20, c = 'blue', label = 'Cluster 3')
+        plt.scatter(self.mean_vec[:, 0], self.mean_vec[:, 1], s = 50, c = 'yellow', label = 'Centroids')
+        plt.show()
