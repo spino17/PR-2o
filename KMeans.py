a3b87bda9e6d2f1957b20e3f1ad52afb602f8aee
Bhavya Bhatt <bhavyabhatt17@gmail.com>
2018-10-07 13:10:16 +0530

    Update KMeans.py

--- KMeans.py
+++ KMeans.py
@@ -8,6 +8,7 @@ class KMeans:
     x_train = np.array([])
     mean_vec = np.array([])
     z = np.array([])
+    N_z = np.array([])
     vec_dim = 0
     total_size = 0
     
@@ -17,16 +18,17 @@ class KMeans:
         self.x_train = x_train
         self.mean_vec = np.zeros(shape = (k, np.size(x_train[0])))
         self.z = np.zeros(np.size(self.x_train))
+        self.N_z = np.zeros(self.k)
         self.vec_dim = np.size(x_train[0])
         self.total_size = int(np.size(x_train) / self.vec_dim)
         
         
-    
+    # euclidean distance measure
     def DistMeasure(self, x1, x2):
         
         return math.sqrt(np.dot(x1 - x2, x1 - x2))
     
-    
+    # function returns cluster number of the data point accor. to Euc. Distance
     def MinDistCluster(self, x_vec, mean_vec):
         
         min_dist = self.DistMeasure(x_vec, mean_vec[0])
@@ -39,18 +41,20 @@ class KMeans:
                 
         return cluster_num
     
-    
+    # fit the model on the training dataset and return optimal k means distribution
     def fit(self, iterations, precision):
 
+        dataset = self.x_train
+        np.random.shuffle(dataset)
+        num = int(self.total_size / self.k)
         for i in range(0, self.k):
-            kth_mean = random.sample(range(0, 500), self.vec_dim)
-            self.mean_vec[i] = kth_mean
+            self.mean_vec[i] = dataset[i * num + int(num / 2)]
         
         iter_num = 0
         cost_func_i = 1000000
         cost_func_f = 0
         while cost_func_i - cost_func_f > precision:
-            N_z = np.zeros(self.k)
+            self.N_z = np.zeros(self.k)
             new_mean_vec = np.zeros(shape = (self.k, self.vec_dim))
             cost_func_i = cost_func_f
             
@@ -58,17 +62,17 @@ class KMeans:
                 cluster_num = self.MinDistCluster(self.x_train[i], self.mean_vec)
                 new_mean_vec[cluster_num] = new_mean_vec[cluster_num] + self.x_train[i]
                 self.z[i] = cluster_num
-                N_z[cluster_num] = N_z[cluster_num] + 1
+                self.N_z[cluster_num] = self.N_z[cluster_num] + 1
                 
             cost_term1_f = 0
             cost_term2_f = 0
                 
             for i in range(0, self.k):
-                if (N_z[i] == 0):
+                if (self.N_z[i] == 0):
                     new_mean_vec[i] = new_mean_vec[i]
                 else:
-                    new_mean_vec[i] = (1 / N_z[i]) * new_mean_vec[i]
-                cost_term1_f = cost_term1_f + np.dot(new_mean_vec[i], new_mean_vec[i]) * N_z[i]
+                    new_mean_vec[i] = (1 / self.N_z[i]) * new_mean_vec[i]
+                cost_term1_f = cost_term1_f + np.dot(new_mean_vec[i], new_mean_vec[i]) * self.N_z[i]
 
             for i in range(0, self.total_size):
                 cost_term2_f = cost_term2_f + np.dot(self.x_train[i], new_mean_vec[int(self.z[i])])
@@ -79,12 +83,28 @@ class KMeans:
             iter_num = iter_num + 1
             print("iternation no: %d diff: %d" % (iter_num, cost_func_i - cost_func_f))
             
-        #print(N_z[0], N_z[1], N_z[2])
-        print(N_z)
+        print(self.N_z)
         print(self.mean_vec)
-        print(abs(cost_func_i - cost_func_f))
-            
-            
+        print(cost_func_i - cost_func_f)
+    
+    # returns the covariance matrix of k clusters
+    def CovMatrix(self):
+        cov_mat_array = np.zeros((self.k, self.vec_dim, self.vec_dim))
+        for i in range(0, self.total_size):
+            cov_mat_array[int(self.z[i])] = cov_mat_array[int(self.z[i])] + np.outer(self.x_train[i] - self.mean_vec[int(self.z[i])], self.x_train[i] - self.mean_vec[int(self.z[i])])
+        
+        for i in range(0, self.k):
+            cov_mat_array[i] = (1 / self.N_z[i]) * cov_mat_array[i]
+        
+        return cov_mat_array
+    
+    # returns initial mixture coefficient for GMM model
+    def MixtureCoeff(self):
+        
+        total_num = np.sum(self.N_z)
+        return (1 / total_num) * self.N_z
+        
+    # predict the cluster of data points after training has occured
     def ClusterPredict(self, X):
         pred_arr = np.array([])
         for x_vec in X:
@@ -92,7 +112,7 @@ class KMeans:
 
         return pred_arr
     
-    
+    # returns the BoVW representation of image patches
     def BoVW(self, mean_vec, img_vec):
         bag = np.zeros(self.k)
         for vec in img_vec:
@@ -101,8 +121,8 @@ class KMeans:
             
         return bag
         
-    
-    def PlotCluster(self): # for 2-D feature vectors
+    # plot the cluster for 2-D dimensional data points
+    def PlotCluster(self):
         y_pred = self.ClusterPredict(self.x_train)
         plt.scatter(self.x_train[y_pred == 0, 0], self.x_train[y_pred == 0, 1], s = 20, c = 'red', label = 'Cluster 1')
         plt.scatter(self.x_train[y_pred == 1, 0], self.x_train[y_pred == 1, 1], s = 20, c = 'green', label = 'Cluster 2')
